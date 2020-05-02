3e50f0c7bf053eecc32d5c29d5420d7fab65280b
Tiago <deoliveiratiago@outlook.com>
2020-05-01 21:17:00 -0300

    Project created

---
+++ src/main/java/ga/deoliveiratiago/ctrlcomercial/CtrlcomercialApplication.java
@@ -0,0 +1,13 @@
+package ga.deoliveiratiago.ctrlcomercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlcomercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlcomercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ga/deoliveiratiago/ctrlcomercial/CtrlcomercialApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.deoliveiratiago.ctrlcomercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlcomercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
