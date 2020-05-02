189c957879909eaa155d45da7395d56f62c861c3
Tiago <deoliveiratiago@outlook.com>
2020-05-02 01:29:45 -0300

    Produto created

--- src/main/java/ga/deoliveiratiago/ctrlcomercial/CtrlcomercialApplication.java
+++ src/main/java/ga/deoliveiratiago/ctrlcomercial/CtrlcomercialApplication.java
@@ -1,13 +1,49 @@
 package ga.deoliveiratiago.ctrlcomercial;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CtrlcomercialApplication {
+import ga.deoliveiratiago.ctrlcomercial.domain.Categoria;
+import ga.deoliveiratiago.ctrlcomercial.domain.Produto;
+import ga.deoliveiratiago.ctrlcomercial.repositories.CategoriaRepository;
+import ga.deoliveiratiago.ctrlcomercial.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class CtrlcomercialApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CtrlcomercialApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
