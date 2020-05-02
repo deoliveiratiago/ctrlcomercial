ec439182b31442fa561710b0e858b2b9c2b50e36
Tiago <deoliveiratiago@outlook.com>
2020-05-02 16:52:02 -0300

    Categoria exceptions handling

--- src/main/java/ga/deoliveiratiago/ctrlcomercial/services/CategoriaService.java
+++ src/main/java/ga/deoliveiratiago/ctrlcomercial/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import ga.deoliveiratiago.ctrlcomercial.domain.Categoria;
 import ga.deoliveiratiago.ctrlcomercial.repositories.CategoriaRepository;
+import ga.deoliveiratiago.ctrlcomercial.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
